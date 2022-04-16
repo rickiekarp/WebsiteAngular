1f9fff7efff978b96860f1a4a5f65248248949e8
Rickie Karp <rickie.karp@gmail.com>
2022-04-16 19:19:10 +0200

    Migrate WebsiteAngular from monorepo

---
+++ src/app/model/project.model.ts
@@ -0,0 +1,13 @@
+export class ProjectDto {
+    name: string;
+    description: string;
+    projectUrl: string;
+    downloadUrl: string;
+    projectImage: string;
+    projectDuration: string;
+    company: string;
+    function: string;
+    text: string;
+    trailerUrl: string;
+    tasks = [];
+}
